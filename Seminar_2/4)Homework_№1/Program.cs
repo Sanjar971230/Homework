a38e90236e8f17a243d3d146a2830c422e330068
sanjar <sanjar12129@gmail.com>
2023-05-27 23:33:35 +0600

    homework 1 не обизательное

---
+++ Seminar_2/4)Homework_№1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача необязательная 1: на входе целое или вещественное число, надо удалить вторую цифру слева этого числа.
+
+// 145 -> 15
+// 1 -> нет
+// 567,123 -> 57,123
+
+
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number <= 999)
+
+    while (number > 100)
+
+        number = number / 10 % 10;
+
+else if (number > 1000)
+
+    number = number / 100 % 10;
+
+System.Console.WriteLine("вторая число" + number);

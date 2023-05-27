c2bd830c1b73c7031b14177afaea2e0face58b0f
sanjar <sanjar12129@gmail.com>
2023-05-27 22:49:50 +0600

    homework 13

---
+++ Seminar_2/2)Homework_№13/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+Console.WriteLine("Введите цисло");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = -1;
+
+if (num >= 100)
+{
+    while (num > 100)
+    {
+        num = num % 100;
+        num = num % 10;
+    }
+    result = num;
+    System.Console.WriteLine("Третье число" + result);
+}
+
+else 
+{
+    System.Console.WriteLine("Третье число нет");
+}

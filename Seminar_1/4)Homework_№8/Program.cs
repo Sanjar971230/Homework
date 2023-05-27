2b6852f9ef6e040da68fbd87c3348ca84559b9b6
sanjar <sanjar12129@gmail.com>
2023-05-27 20:27:37 +0600

    homework 8

---
+++ Seminar_1/4)Homework_№8/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 2;
+
+if (number > 1)
+{
+    while (result <= number)
+    {
+        System.Console.WriteLine(result + " Четное число");
+        result = result + 2;
+    }
+
+}

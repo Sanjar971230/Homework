a80bf90ecaffbd1ac0cdfad7dce4c471e78f908c
sanjar <sanjar12129@gmail.com>
2023-05-31 12:42:20 +0600

    задание №19

---
+++ Seminar_3/1)Homework_№19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+System.Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int cub, sum = 0, result;
+
+if
+  (num > 9999 && num > 9999)
+{
+    for (result = num; num != 0; num = num / 10)
+    {
+        cub = num % 10;
+        sum = sum * 10 + cub;
+    }
+    if (result == sum)
+
+        System.Console.WriteLine("Да + {result}");
+
+    else
+
+        System.Console.WriteLine("Нет+ {result}");
+
+}

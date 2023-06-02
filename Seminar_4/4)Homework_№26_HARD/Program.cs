617cc9d25c187b710720adf22604183aec833db2
sanjar <sanjar12129@gmail.com>
2023-06-02 22:59:58 +0600

    задание 29,26

---
+++ Seminar_4/4)Homework_№26_HARD/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 26 HARD Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
+
+
+System.Console.WriteLine("Введите число");
+int sum = int.Parse(Console.ReadLine());
+
+
+int result = 0;
+
+while (sum > 0)
+{
+    result++;
+    sum=sum / 10;
+}
+
+System.Console.WriteLine("Колличество цифр" + result);
